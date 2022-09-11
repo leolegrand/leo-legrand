9c7512f070c817bba42824f716751378fabca636
leolegrand <88097360+leolegrand@users.noreply.github.com>
2022-09-11 06:56:25 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="title">Site web en cours de création</h2>
+      <p className="mail">
+        {' '}
+        Vous pouvez me contacter à cette adresse : leo.legrandm@gmail.com
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
